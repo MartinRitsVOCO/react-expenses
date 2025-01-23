ea988db7947bb149db1e97af86743dd7cfb5ca3e
MartinRitsVOCO <martin.ritsberg@voco.ee>
2025-01-23 13:04:23 +0200

    Ülesanne 3 - ümbritseva komponendi rakendamine teistes komponentides

--- react-expenses/src/components/ExpensesItem.jsx
+++ react-expenses/src/components/ExpensesItem.jsx
@@ -1,18 +1,19 @@
 /* eslint-disable react/prop-types */
 import './ExpensesItem.css';
-import './Card.css'
 import ExpensesDate from './ExpensesDate';
+import Card from './Card';
 
-const ExpensesItem = (props) => {
+const ExpensesItem = ({data}) => {
+    const {date, title, price} = data;
 
     return (
-        <div className='card expense-item'>
-            <ExpensesDate date={props.data.date} />
+        <Card className='expense-item'>
+            <ExpensesDate date={date} />
             <div className='expense-item__description'>
-                <h2>{props.data.title}</h2>
-                <div className='expense-item__price'>{props.data.price}</div>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>{price}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
